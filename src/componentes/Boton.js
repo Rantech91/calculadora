fac4c4289835610e90596e33ea49f2a46473d80e
Jose Recart <joserecart91@gmail.com>
2022-09-26 20:15:05 -0300

    update

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -9,7 +9,7 @@ function Boton(props) {
 
     return(
         <div
-          className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}>
+          className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()} onClick={ () => props.manejarClic(props.children)}>
           {props.children}
         </div>
     )
